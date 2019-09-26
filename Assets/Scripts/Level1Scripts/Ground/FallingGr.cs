860450624f3956eb0f32769341dbcb5ea5bdf5b3
silver2312 <38344257+silver2312@users.noreply.github.com>
2019-09-26 21:00:27 +0700

    master
    26/9

---
+++ Assets/Scripts/Level1Scripts/Ground/FallingGr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGr : MonoBehaviour {
+	public Rigidbody2D g2;
+	public float timedelay = 0.5f;
+	// Use this for initialization
+	void Start () {
+		g2 = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	private void OnCollisionEnter2D(Collision2D col) {
+		if(col.collider.CompareTag("Player"))
+		{
+			StartCoroutine(fall());
+		}
+	}
+	IEnumerator fall()
+	{
+		yield return new WaitForSeconds(timedelay);
+		g2.bodyType = RigidbodyType2D.Dynamic;
+		yield return 0;
+	}
+}
